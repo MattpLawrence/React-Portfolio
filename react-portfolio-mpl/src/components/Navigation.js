5de02860fad1f28dc02d91dd4343711e69364147
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-27 15:12:41 -0500

    Update Navigation.js

--- react-portfolio-mpl/src/components/Navigation.js
+++ react-portfolio-mpl/src/components/Navigation.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navigation() {
+  const [pageNav, setPageNav] = useState("About");
+
+  const changePage = (e) => {
+    const name = e.target.name;
+    console.log(name);
+  };
+
   return (
     <div className="container-fluid">
       <a className="navbar-brand" href="#">
@@ -20,17 +27,27 @@ function Navigation() {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link " href="#">
+            <a className="nav-link " href="#" onClick={changePage} name="About">
               About Me
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a
+              className="nav-link"
+              href="#"
+              onClick={changePage}
+              name="Project"
+            >
               Projects
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">
+            <a
+              className="nav-link"
+              href="#"
+              onClick={changePage}
+              name="Contact"
+            >
               Contact
             </a>
           </li>
