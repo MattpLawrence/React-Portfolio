7ddb4b65bd9ee7fecdf02627365a0b9db824a399
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-27 16:44:03 -0500

    Update projectList.js

--- react-portfolio-mpl/src/components/subComponents/projectList.js
+++ react-portfolio-mpl/src/components/subComponents/projectList.js
@@ -1,8 +1,39 @@
 const projectList = [
   {
+    imgSrc: "images/gamerGabble.png",
+    imgAlt: "Gamer's Chat Application",
     appHref: "https://gamer-gabble.herokuapp.com/",
     gitHref: "https://github.com/MattpLawrence/Game-Chat",
     name: "Gamer Chat Application",
+    description:
+      "This application uses Node.js and Express.js to establish the backend server and routes to enable a fully functioning social media-style chat app. Using MySQL and JawsDB user information is stored and used for authentication. The application was built using an MVC organizational structure and a CRUD database design. This application was designed to be fully mobile-friendly.",
+  },
+  {
+    imgSrc: "images/lrgWeather.png",
+    imgAlt: "Weather Forecast Application",
+    appHref: "https://mattplawrence.github.io/Weather-Dashboard/",
+    gitHref: "https://github.com/MattpLawrence/Weather-Dashboard",
+    name: "Weather Dashboard",
+    description:
+      "A one page dashboard were you can search by city to find the weather. The page displays today's weather in depth as well as the upcoming five day forecast.",
+  },
+  {
+    imgSrc: "images/gamerGabble.png",
+    imgAlt: "YouTube Music Application",
+    appHref: "https://github.com/MattpLawrence/YouTube_Music_Finder",
+    gitHref: "https://github.com/MattpLawrence/YouTube_Music_Finder",
+    name: "YouTube Music Player",
+    description:
+      "A music player that generates random songs, saves your view and search history. Worked with a group using AGILE methodology and SCRUM to accomplish the project. Github branches and Kanban boards were used to keep track of objectives.",
+  },
+  {
+    imgSrc: "images/gamerGabble.png",
+    imgAlt: "Javascript Code Quiz Application",
+    appHref: "https://github.com/MattpLawrence/Code-Quiz",
+    gitHref: "https://github.com/MattpLawrence/Code-Quiz",
+    name: "Javascript Code Quiz",
+    description:
+      "This application uses vanilla JavaScript and jQuery to manipulate the DOM and produce a responsive online quiz. Using local storage the quiz stores your score, and compares it with previous high scores. Styled with CSS and initialized in HTML, this demonstrates a complete, functioning timed quiz.",
   },
 ];
 
