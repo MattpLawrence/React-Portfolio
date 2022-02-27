2a2821254591687d26b0bc3231a82c1cfd9356f0
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-27 16:26:27 -0500

    add useEffect to change title

--- react-portfolio-mpl/src/components/Page.js
+++ react-portfolio-mpl/src/components/Page.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import About from "./About";
 import Footer from "./Footer";
@@ -7,19 +7,28 @@ import Project from "./Project";
 import Contact from "./Contact";
 
 function Page() {
+  // set state to pass down to nav
   const [pageNav, setPageNav] = useState("About");
-
+  //function to change page based on what nav tab is clicked
   const changePage = (e) => {
     const name = e.target.name;
     setPageNav(name);
   };
+  // conditionally render correct page based on nav state
   const renderPage = () => {
     if (pageNav == "About") {
       return <About />;
     } else if (pageNav == "Project") {
       return <Project />;
+    } else if (pageNav == "Contact") {
+      return <Contact />;
+    } else {
+      return <h1> Page not found</h1>;
     }
   };
+  useEffect(() => {
+    document.title = `${pageNav} MPL`;
+  });
 
   return (
     <div>
