c01bc68bc26aee19e072c4e0bd5df88d4662f2a0
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-28 10:08:50 -0500

    font awesome working, moving to styling.

--- react-portfolio-mpl/src/components/Contact.js
+++ react-portfolio-mpl/src/components/Contact.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
     <div>
       <ul>
         <li className="contactLI">
-          <i className="fas fa-envelope contactIcon"></i>
+          <FontAwesomeIcon icon={faEnvelope} className="fa" />
           MattphLawrence@gmail.com
         </li>
         <li className="contactLI">
-          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          <FontAwesomeIcon icon={faLinkedin} className="fa" />
           <a href="https://www.linkedin.com/in/mattplawrence/" target="_blank">
             {" "}
             MattpLawrence
           </a>
         </li>
         <li className="contactLI">
-          <i className="fab fa-github contactIcon"></i>
+          <FontAwesomeIcon icon={faGithub} className="fa" />
           <a href="https://github.com/MattpLawrence" target="_blank">
             MattpLawrence
           </a>
