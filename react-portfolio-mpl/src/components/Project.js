7f4a8ff862cc39a2b788596018ee297ea1cd6d82
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-28 08:23:32 -0500

    Update Project.js

--- react-portfolio-mpl/src/components/Project.js
+++ react-portfolio-mpl/src/components/Project.js
@@ -2,13 +2,60 @@ import React from "react";
 import projectList from "../assets/projectList";
 
 function Project() {
-  return (
-    <div>
-      {projectList.map((proj) => {
-        <div></div>;
-      })}
+  // let projectCards = projectList.map((proj) => {
+  //   // destructure
+  //   const { imgSrc, imgAlt, appHref, gitHref, name, description } = proj;
+  //   // fil out jsx card
+  //   <div>
+  //     <div className="card" style="width: 18rem;">
+  //       <img
+  //         src={imgSrc}
+  //         className="card-img-top"
+  //         alt={imgAlt}
+  //         href={appHref}
+  //       ></img>
+  //       <div className="card-body">
+  //         <h5 className="card-title">{name}</h5>
+  //         <p className="card-text">{description}</p>
+  //       </div>
+  //       <ul className="list-group list-group-flush">
+  //         <li className="list-group-item">An item</li>
+  //         <li className="list-group-item">A second item</li>
+  //         <li className="list-group-item">A third item</li>
+  //       </ul>
+  //       <div className="card-body">
+  //         <a href={gitHref} className="card-link">
+  //           Card link
+  //         </a>
+  //       </div>
+  //     </div>
+  //   </div>;
+
+  //   return projectCards;
+  // });
+
+  return projectList.map((proj) => (
+    <div key={i}>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src={imgSrc}
+          className="card-img-top"
+          alt={imgAlt}
+          href={appHref}
+        ></img>
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{description}</p>
+        </div>
+
+        <div className="card-body">
+          <a href={gitHref} className="card-link">
+            Card link
+          </a>
+        </div>
+      </div>
     </div>
-  );
+  ));
 }
 
 export default Project;
